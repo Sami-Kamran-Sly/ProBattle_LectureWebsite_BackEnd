d8bfa1ecff70f7185c861560b221dc93f0b8f500
Sami kamran <130062857+Sami-Kamran-Sly@users.noreply.github.com>
2025-04-09 22:03:51 +0500

    Update CAN HAVE BIGGER VIDEO FILE SIZE 15mb limit only  LectureRoutes.js

--- routes/LectureRoutes.js
+++ routes/LectureRoutes.js
@@ -20,7 +20,7 @@ const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB limit
+    fileSize: 15 * 1024 * 1024, // 5MB limit
   },
 });
 
